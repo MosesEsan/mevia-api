77df03b1e73118bdd390b0b5dfdaf2e39f302eee
Moses Esan <esanm@rte.ie>
2021-12-03 10:00:40 +0000

    Index route fie updated

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const auth = require('./auth');
 const home = require('./home');
 const question = require('./question');
 const question_types = require('./question-types');
+const weekly_prize = require('./weekly_prize');
 const game = require('./game');
 const challenge = require('./challenge');
 const user = require('./user');
@@ -18,22 +19,29 @@ module.exports = app => {
 
     /*
     Serve all files/folders inside of the 'uploads' directory
-    And make them accessible through http://localhost:3000/uploads.
+    And make them accessible through http://localhost:3001/uploads.
     */
     app.use(express.static(__dirname + '/public'));
     app.use('/uploads', express.static('uploads'));
 
-    app.use('/api/home', authenticate, home);
 
+    //ADMIN ROUTES
+    app.use('/api/admin/user', user);
+    app.use('/api/admin/question', question);
+    app.use('/api/admin/questionTypes', question_types);
+    app.use('/api/admin/weeklyPrize', weekly_prize);
+    app.use('/api/admin/challenge', challenge);
+    app.use('/api/admin/leaderboard', leaderboard);
+    app.use('/api/admin/prize', prize);
+
+    //USER ROUTES
     app.use('/api/auth', auth);
-    app.use('/api/question', question);
-    app.use('/api/questionTypes', question_types);
-
-    app.use('/api/leaderboard', leaderboard);
-
-    app.use('/api/game', authenticate, game);
-    app.use('/api/challenge', authenticate, challenge);
-
+    app.use('/api/home', authenticate, home);
     app.use('/api/user', authenticate, user);
+    app.use('/api/challenge', authenticate,challenge);
+    app.use('/api/game', authenticate, game);
+    app.use('/api/leaderboard', authenticate, leaderboard);
+
     app.use('/api/prize', authenticate, prize);
+    // app.use('/api/prize', prize);
 };
