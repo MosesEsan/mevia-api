0e6d4923f844e9e698b55e5758bd2c1d9ebaca9a
Moses Esan <esanm@rte.ie>
2021-12-05 00:49:26 +0000

    refactored code

--- src/controllers/leaderboard.js
+++ src/controllers/leaderboard.js
@@ -13,6 +13,31 @@ exports.index = async function (req, res) {
     try {
         let type = (req.body.type) ? req.body.type : "week";
 
+        let leaderboard = await runQuery(type)
+
+        // if (leaderboard.length === 0 && type === "week"){
+        //     type = "month"
+        //     leaderboard = await runQuery("month")
+        // }
+
+        let top_leaders = []
+        leaderboard.map((user, idx) => {
+            user['id'] = idx + 1;
+            user['rank'] = idx + 1;
+            if (idx < 3) top_leaders.push(user)
+        })
+
+
+        res.status(200).json({success: true, data: {leaderboard, top_leaders, title: `Top leaders of the ${type}`}});
+    } catch (error) {
+        logger.error(e);
+        res.status(500).json({error})
+    }
+};
+
+
+async function runQuery(type) {
+    try {
         const today = moment();
         let startOfWeek = today.startOf('week').format('YYYY-MM-DD HH:mm');
         let endOfWeek = today.endOf('week').format('YYYY-MM-DD HH:mm');
@@ -27,12 +52,6 @@ exports.index = async function (req, res) {
 
             start = moment(startOfDay)
             end = moment(endOfDay)
-
-            console.log("Start")
-            console.log(start)
-            console.log(end)
-            console.log("End")
-
         } else if (type === "month") {
             // Month
             let startOfMonth = moment().startOf('month').format('YYYY-MM-DD HH:mm');
@@ -42,17 +61,13 @@ exports.index = async function (req, res) {
             end = moment(endOfMonth)
         }
 
-
-        console.log(new Date(start))
-        console.log(new Date(end))
-
-        const leaderboard = await prisma.$queryRaw`
-            SELECT user.id as user_id, user.username, COALESCE(points_obtained, 0) AS points, COALESCE(points_available, 0) AS points_available, 
+    const leaderboard = await prisma.$queryRaw`
+            SELECT user.id as user_id, user.username, user.image, COALESCE(points_obtained, 0) AS points, COALESCE(points_available, 0) AS points_available, 
             COALESCE(no_of_games_played, 0) AS no_of_games_played,
             (points_obtained/no_of_games_played) as average
             FROM user
             INNER JOIN 
-            (SELECT game.userId as user_id, user.username, SUM((CASE WHEN gq.correct is True THEN qt.points ELSE 0 END)) as points_obtained
+            (SELECT game.userId as user_id, user.username, user.image, SUM((CASE WHEN gq.correct is True THEN qt.points ELSE 0 END)) as points_obtained
             FROM game_question gq
             INNER JOIN game  ON gq.gameId = game.id 
             INNER JOIN question ON gq.questionId = question.id 
@@ -75,27 +90,14 @@ exports.index = async function (req, res) {
             AND game.initiatedAt BETWEEN ${new Date(start)} AND ${new Date(end)}
             GROUP BY userId) points_available ON points_available.userId = user.id
             ORDER BY points desc, no_of_games_played desc 
-            LIMIT 10`
+            LIMIT 10`;
 
-        console.log("leaderboard")
-        console.log(leaderboard)
-        console.log("leaderboard")
+    return leaderboard;
 
-
-        let top_leaders = []
-        leaderboard.map((user, idx) => {
-            user['id'] = idx + 1;
-            user['rank'] = idx + 1;
-            if (idx < 3) top_leaders.push(user)
-        })
-
-        res.status(200).json({success: true, data: {leaderboard, top_leaders}});
-    } catch (error) {
-        logger.error(e);
-        res.status(500).json({error})
-    }
-};
-
+} catch (error) {
+        throw error;
+}
+}
 
 // UNION
 // (SELECT
