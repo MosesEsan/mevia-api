96a37624776db89881727210006ba4dbe120aab3
Moses Esan <esanm@rte.ie>
2022-02-23 01:09:44 +0000

    updated getNextGameTime to add the date to the next game at

--- src/utils/check-game.js
+++ src/utils/check-game.js
@@ -20,10 +20,19 @@ exports.checkGame = async function (challengeIdentifier, user_id) {
     }
 }
 
-
 const format = 'HH:mm:ss'
 
 exports.getNextGameTime = function (challengeEndTime) {
+    let check = moment()
+    let month = check.format('M'),
+        date = check.format('D'),
+        year = check.format('YYYY')
+
+    challengeEndTime = moment(challengeEndTime)
+    challengeEndTime.set('year', year);
+    challengeEndTime.set('month', month-1);
+    challengeEndTime.set('date', date);
+
     //calculate the time for the next game
     let timePlusThirty = moment().add(30, 'minutes')
     timePlusThirty = moment(timePlusThirty, format)
@@ -36,31 +45,6 @@ exports.getNextGameTime = function (challengeEndTime) {
 
     return nextGame;
 }
-exports.getNextTournamentGameTime = function (tournamentEndTime) {
-    let check = moment()
-    let month = check.format('M'),
-        date = check.format('D'),
-        year = check.format('YYYY')
-
-    tournamentEndTime = moment(tournamentEndTime)
-    tournamentEndTime.set('year', year);
-    tournamentEndTime.set('month', month-1);
-    tournamentEndTime.set('date', date);
-
-    //calculate the time for the next game
-    let timePlusThirty = moment().add(30, 'minutes')
-    timePlusThirty = moment(timePlusThirty, format)
-
-    let nextGame = null;
-    //If the next game is before the end of the current challenge
-    if (timePlusThirty.isBefore(tournamentEndTime)) {
-        nextGame = new Date(timePlusThirty.format("YYYY-MM-DDTHH:mm:ss.000Z"));
-    }
-
-    return nextGame;
-}
-
-
 
 exports.checkAnswers = function (questions) {
     let points_obtained = 0;
